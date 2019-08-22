d018700a1d8219fe8cbe7b8290de8c4bdec5dc9a
Felipe Vieira <felipemartim@gmail.com>
2019-08-21 21:17:18 -0300

    flask microservice

---
+++ plataformas_cognitivas/app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, make_response, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+def results():
+    # req = request.get_json(force=True)
+    # action = req.get('queryResult').get('action')
+    return {'fulfillmentText': 'This is a response from webhook.'}
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    return make_response(jsonify(results()))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
